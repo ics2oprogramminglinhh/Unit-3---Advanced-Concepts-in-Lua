525707d13490f72436c2d5d612d0d9ebe46b12b5
s272936 <46288798+s272936@users.noreply.github.com>
2019-05-10 11:52:12 -0400

    Update Math.lua

--- SampleVideoGameLinh/Math.lua
+++ SampleVideoGameLinh/Math.lua
@@ -9,9 +9,6 @@
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- set the background colour
-display.setDefault("background", 107/255, 143/255, 221/255)
-
 -- Use Composer Library
 local composer = require( "composer" )
 
@@ -28,9 +25,9 @@ sceneName = "Math"
 -- Creating Scene Object
 local scene = composer.newScene( sceneName )
 
------------------------------------------------------------------------------------------
--- LOCAL VARIABLES
------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+-- LOCAL VARIABLES 
+------------------------------------------------------------------------------------------
 
 -- Create local variables
 local questionObject
@@ -40,34 +37,18 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
-local randomNumber3
-local randomNumber4
 
 -- Additional local variables
 local incorrectObject
 local pointsObject
 local points = 0
 local randomOperator
-
-
--- Variables for the timer
-local totalSeconds = 10
-local secondsLeft = 10
-local clockText
-local countDownTimer
-
-local lives = 3
-local heart1
-local heart2
-local heart3
+local randomNumber3
+local randomNumber4
 
 -------------------------------------------------------------------------------------------
 -- SOUNDS
 -------------------------------------------------------------------------------------------
-
-local youLose
-local youLoseSound = audio.loadSound("Sounds/gameOver.mp3")
-local youLoseChannel
 
 -- Correct sound
 local correctSound = audio.loadSound("Sounds/correct.mp3")
@@ -76,19 +57,20 @@ local correctSoundChannel
 -- Wrong Sound
 local wrongSound = audio.loadSound("Sounds/wrong.mp3")
 local wrongSoundChannel
------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 
 local function AskQuestion()
+-- generate a random number between 1 and 4
+    randomOperator = math.random(1, 4)
+
     -- generate 2 random numbers between a max. and a min. number
     randomNumber1 = math.random(10, 20)
     randomNumber2 = math.random(10, 20)
     randomNumber3 = math.random(0, 10)
     randomNumber4 = math.random(0, 10)
-
-    -- changes the signs (-, +, *)
-    randomOperator = math.random(1, 3)
 
     -- display points 
     pointsObject.text = "Points" .. " = " .. points
@@ -115,26 +97,36 @@ local function AskQuestion()
         -- create question in text object
         questionObject.text = randomNumber3 .. " x " .. randomNumber4 .. " = "
     -- otherwise, if the random operator is 4, do multiplication
+    elseif (randomOperator == 4) then
+            correctAnswer = randomNumber3 / randomNumber4
+
+        -- create question in text object
+            questionObject.text = randomNumber3 .. "/" .. randomNumber4 .. "="
     end
 end
 
 local function HideCorrect()
     correctObject.isVisible = false
-    BackTransition()
+    AskQuestion()
 end
 
 local function HideIncorrect()
     incorrectObject.isVisible = false
-    BackTransition()
-end
-
-local function YouLoseTransition()
-    composer.gotoScene( "you_Lose", {effect = "zoomInOutFade", time = 1000})
+    AskQuestion()
 end
 
 -- Creating Transitioning Function back to main menu
-local function BackTransition( )
+local function BackTransition()
     composer.gotoScene( "level1_screen", {effect = "zoomInOutFade", time = 500})
+    numericField = false
+end
+
+-- Creating transitioning funtion to restart when you win or you lose
+local function Restart()
+    NumericFieldListener()
+    UpdateTime()
+    lives = 3
+
 end
 
 local function NumericFieldListener(event)
@@ -155,81 +147,19 @@ local function NumericFieldListener(event)
             correctObject.isVisible = true
             incorrectObject.isVisible = false
             correctSoundChannel = audio.play(correctSound)
-            timer.performWithDelay(2500, HideCorrect)
+            timer.performWithDelay(1500, HideCorrect)
             points = points + 1
             pointsObject.text = "Points" .. " = ".. points
-            BackTransition()
 
         elseif (userAnswer) then
             correctObject.isVisible = false
             incorrectObject.isVisible = true
             wrongSoundChannel = audio.play(wrongSound)
-            timer.performWithDelay(2500, HideIncorrect)
-            lives = lives - 1
+            timer.performWithDelay(1500, HideIncorrect)
             BackTransition()
 
-    elseif (lives == 3) then
-        heart1.isVisible = true
-        heart2.isVisible = true
-        heart3.isVisible = true
-    elseif (lives == 2) then
-        heart1.isVisible = true
-        heart2.isVisible = true
-        heart3.isVisible = false
-    elseif (lives == 1) then
-        heart1.isVisible = true
-        heart2.isVisible = false
-        heart3.isVisible = false
-    elseif (lives == 0) then
-        heart1.isVisible = false
-        heart2.isVisible = false
-        heart3.isVisible = false
-            youLose.isVisible = true
-            numericField.isVisible = false
-            clockText.isVisible = false
-            youLoseSoundChannel = audio.play(youLose)
-        
         end
-    -- clear text field
-    event.target.text = ""
-    end
-
-end
-
-local function UpdateTime()
-
-    -- decrement the number of seconds
-    secondsLeft = secondsLeft - 1
-
-    -- display the number of seconds left in the clock object
-    clockText.text = secondsLeft .. ""
-
-    if (secondsLeft == 0) then
-        -- reset the number of seconds left
-        secondsLeft = totalSeconds 
-        lives = lives - 1
-        AskQuestion()
-
-        if (lives == 3) then
-            heart3.isVisible = false
-        elseif (lives == 2) then
-            heart2.isVisible = false
-        elseif (lives == 1) then
-            heart1.isVisible = false
-            youLose.isVisible = true
-            numericField.isVisible = false
-            clockText.isVisible = false
-            youLoseSoundChannel = audio.play(youLose)
-            -- clear text field
-            event.target.text = ""
-        end
-    end
-end
-
--- function that calls the timer
-local function StartTimer()
-    -- create a countdown timer that loops infinitely
-    countDownTimer = timer.performWithDelay(1500, UpdateTime, 0)
+    end 
 end
 
 ------------------------------------------------------------------------------------------
@@ -257,33 +187,28 @@ incorrectObject.isVisible = false
 numericField = native.newTextField(500, display.contentWidth/2, display.contentHeight/2, 100, 100)
 numericField.inputType = "number"
 
--- create the lives to display on the screen
-heart1 = display.newImageRect("Images/heart.png", 100, 100)
-heart1.x = display.contentWidth * 9.5 / 10
-heart1.y = display.contentHeight * 1 / 8
-
-heart2 = display.newImageRect("Images/heart.png", 100, 100)
-heart2.x = display.contentWidth * 8.5 / 10
-heart2.y = display.contentHeight * 1 / 8
-
-heart3 = display.newImageRect("Images/heart.png", 100, 100)
-heart3.x = display.contentWidth * 7.5 / 10
-heart3.y = display.contentHeight * 1 / 8
-
--- display the timer
-clockText = display.newText("", 100, 100, Arial, 50)
-clockText:setTextColor(204/255, 0/255, 0/255)
-
--- youLose Screen
-youLose = display.newImageRect("Images/youLose.png", 1100, 1100)
-youLose.x = 500
-youLose.y = 400
-youLose.isVisible = false
-
 -- add the event listener for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener )
 
 -----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    ----------------------------------------------------------------------------------
+    --adding objects to the scene group
+    ----------------------------------------------------------------------------------
+ 
+    sceneGroup:insert(questionObject)
+    sceneGroup:insert(pointsObject)
+    sceneGroup:insert(correctObject)
+    sceneGroup:insert(incorrectObject)
+    sceneGroup:insert( numericField )
+
+end --function scene:create( event )
 
 -----------------------------------------------------------------------------------------
 
@@ -292,30 +217,32 @@ function scene:show( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
-
-    -----------------------------------------------------------------------------------------
-
     local phase = event.phase
 
     -----------------------------------------------------------------------------------------
 
-    -- Called when the scene is still off screen (but is about to come on screen).   
     if ( phase == "will" ) then
-       
-    -----------------------------------------------------------------------------------------
 
-    -- Called when the scene is now on screen.
-    -- Insert code here to make the scene come alive.
-    -- Example: start timers, begin animation, play audio, etc.
-    elseif ( phase == "did" ) then       
-        -- Play the background music for this scene
-        youLoseChannel = audio.play(youLose)
-        heart1.isVisible = true
-        heart2.isVisible = true
-        heart3.isVisible = true
-        clockText.isVisible = true
+        -- Called when the scene is still off screen (but is about to come on screen).    
+        
+
+    elseif ( phase == "did" ) then
+
+        -- Called when the scene is now on screen.
+        -- Insert code here to make the scene come alive.
+        -- Example: start timers, begin animation, play audio, etc.
+        AskQuestion()
+
     end
-end -- function scene:show( event )
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- Custom function for resuming the game (from pause state)
+function scene:resumeGame()
+   composer.hideOverlay( "fade", 400)
+end
 
 -----------------------------------------------------------------------------------------
 
@@ -332,12 +259,7 @@ function scene:hide( event )
         -- Called when the scene is on screen (but is about to go off screen).
         -- Insert code here to "pause" the scene.
         -- Example: stop timers, stop animation, stop audio, etc.
-        audio.stop(correctSound)
-        heart1.isVisible = false
-        heart2.isVisible = false
-        heart3.isVisible = false
-        clockText.isVisible = false
-        numericField:removeEventListener("userInput", NumericFieldListener)
+        
 
     -----------------------------------------------------------------------------------------
 
